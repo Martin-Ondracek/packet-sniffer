c00925a0b892b7168a729a70f429f80c34cf798e
Martin-Ondracek <martinondracek0@gmail.com>
2022-04-03 21:00:05 +0200

    dekodovanie http paketu

--- src/main/java/org/pevs/App.java
+++ src/main/java/org/pevs/App.java
@@ -1,14 +1,20 @@
 package org.pevs;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 import org.pcap4j.core.*;
 import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
-import org.pcap4j.packet.EthernetPacket;
+import org.pcap4j.packet.*;
 
-import org.pcap4j.packet.IpV4Packet;
-import org.pcap4j.packet.Packet;
+import org.pcap4j.packet.namednumber.IpNumber;
+import org.pcap4j.packet.namednumber.TcpPort;
+import org.pcap4j.util.Inet4NetworkAddress;
 import org.pcap4j.util.NifSelector;
 
 public class App {
@@ -23,8 +29,10 @@ public class App {
         return device;
     }
 
+
     public static void main(String[] args) throws PcapNativeException, NotOpenException {
         // The code we had before
+//        NifSelector nifSelector = new NifSelector();
         PcapNetworkInterface device = getNetworkDevice();
         System.out.println("You chose: " + device);
 
@@ -41,36 +49,67 @@ public class App {
         handle = device.openLive(snapshotLength, PromiscuousMode.PROMISCUOUS, readTimeout);
 
         // filter iba na port 80
-        String filter = "tcp port 80";
-        //handle.setFilter(filter, BpfProgram.BpfCompileMode.OPTIMIZE);
+//        BpfFilterBuilder bpf = new BpfFilterBuilder();
+//        String filter = bpf.setFilter();
+
+        String filter = "port 80";
+//        String filter = "dst host 239.255.255.250 || src host 192.168.1.6";//"dst host 192.168.1.6 || src host 192.168.1.6";//"tcp port 80";
+        handle.setFilter(filter, BpfProgram.BpfCompileMode.OPTIMIZE);
 
         // Create a listener that defines what to do with the received packets
         PacketListener listener = new PacketListener() {
-            //@Override
+            @Override
             public void gotPacket(Packet packet) {
                 // Override the default gotPacket() function and process packet
-                System.out.println(handle.getTimestamp());
-                System.out.println(packet);
-                try{
+//                IpV4Packet.IpV4Header ipV4Header = packet.get(IpV4Packet.class).getHeader();
+//                System.out.println(handle.getTimestamp() + " " + ipV4Header.getSrcAddr() + " > " + ipV4Header.getDstAddr() +" : " + ipV4Header.getProtocol() + ipV4Header.getTotalLength());
+//                IpV4Packet.IpV4Header ipV4Header = packet.get(IpV4Packet.class).getHeader();
+                //System.out.println(ipV4Header.getSrcAddr() + " > " + ipV4Header.getDstAddr() +" : " + ipV4Header.getProtocol() + ipV4Header.getTotalLength());
+                //System.out.println("cely packet" + packet);
+//
 
-                    Inet4Address inet4Address = packet.get(IpV4Packet.class).getHeader().getSrcAddr();
-                    //EthernetPacket ethernetPacket = packet.get(EthernetPacket.class);
-                    //EthernetPacket.EthernetHeader ethernetHeader = ethernetPacket.getHeader();
-                    System.out.println(inet4Address);
-                }catch (Exception e){
-                    e.printStackTrace();
+                IpV4Packet.IpV4Header ipV4Header = packet.get(IpV4Packet.class).getHeader();
+                System.out.println(handle.getTimestamp() + " "
+                        + ipV4Header.getSrcAddr() + " > "
+                        + ipV4Header.getDstAddr() + " : "
+                        + ipV4Header.getProtocol() + " lenght: "
+                        + ipV4Header.getTotalLength()
+                    );
+
+                //System.out.println(packet);
+//                Packet.Header header =packet.getHeader();
+                short port_num = 80;
+                TcpPort httpPort = new TcpPort(port_num,"HTTP");
+//                System.out.println(packet.get(TcpPacket.class).getHeader().getDstPort().equals(httpPort)
+//                + "\n " + httpPort
+//                + "\n " + packet.get(TcpPacket.class).getHeader().getDstPort());
+
+
+                if(packet.get(TcpPacket.class).getHeader().getDstPort().equals(httpPort) ||
+                        packet.get(TcpPacket.class).getHeader().getSrcPort().equals(httpPort)){
+
+                    //packet.get(IpV4Packet.class).getHeader().get
+                    //byte[] data = packet.get(IpV4Packet.class).getPayload().getRawData();
+                    byte[] data = packet.get(TcpPacket.class).getPayload().getRawData();
+                    String decoded = new String(data, StandardCharsets.UTF_8);
+                    System.out.println(decoded);
                 }
+
 
             }
         };
 
         // Tell the handle to loop using the listener we created
         try {
-            int maxPackets = 10;
+            int maxPackets = -1;
             handle.loop(maxPackets, listener);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+//        PcapStat stats = handle.getStats();
+
+//        System.out.println(stats.getNumPacketsCaptured());
 
         // Cleanup when complete
         handle.close();
